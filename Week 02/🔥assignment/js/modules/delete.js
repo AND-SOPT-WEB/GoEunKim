80046fd2b8488954791bfc3d4b12b6393b42b2f2
goeun kim <rlarhdms6828@naver.com>
2024-10-14 13:57:36 +0900

    Fix: 삭제시 id를 기반으로 필터링

--- Week 02/🔥assignment/js/modules/delete.js
+++ Week 02/🔥assignment/js/modules/delete.js
@@ -7,7 +7,8 @@ const deleteSelected = (datas) => {
     selectedIdxes.push(Number(box.getAttribute('data-idx')));
   });
 
-  return datas.filter((_, index) => !selectedIdxes.includes(index));
+  // 인덱스로 받으면 중간 값 삭제시 제대로 동작 x -> id로 필터링
+  return datas.filter((data) => !selectedIdxes.includes(data.id));
 };
 
 export default deleteSelected;
