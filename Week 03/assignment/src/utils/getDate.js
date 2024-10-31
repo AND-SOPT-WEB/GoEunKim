7f0c5b75a9959f0edc77cdcd95baa58c16e722aa
goeun kim <rlarhdms6828@naver.com>
2024-10-31 13:31:54 +0900

    Chore: 시간 포멧팅 주석 변경

--- Week 03/assignment/src/utils/getDate.js
+++ Week 03/assignment/src/utils/getDate.js
@@ -1,12 +1,7 @@
 const getData = () => {
-  // 현재 날짜와 시간을 가져오기
   const currentDate = new Date();
-
-  // 날짜와 시간을 문자열로 포맷팅
+  //시간 형식 포멧팅 (년도-월-일-시:분:초)
   const formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
-
-  console.log(formattedDate);
-  // 포맷팅된 날짜와 시간을 출력
   return formattedDate;
 };
 
