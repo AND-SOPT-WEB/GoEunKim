ceb9cc6bb7a8f99feb7714c533227a130a29eb34
goeun kim <rlarhdms6828@naver.com>
2024-11-01 13:31:17 +0900

    Feat: 기록 포멧팅

--- Week 03/assignment/src/utils/getDate.js
+++ Week 03/assignment/src/utils/getDate.js
@@ -2,8 +2,12 @@
 const getDate = () => {
   const currentDate = new Date();
   //시간 형식 포멧팅 (년도-월-일-시:분:초)
-  const formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+  const formattedDate = `${currentDate.getFullYear()}-${formatting(currentDate.getMonth() + 1)}-${formatting(currentDate.getDate())} ${formatting(currentDate.getHours())}:${formatting(currentDate.getMinutes())}:${formatting(currentDate.getSeconds())}`;
   return formattedDate;
 };
 
+// 월/일/시/분/초를 2자리로 고정! 1분의 경우 -> 01분으로 출력해서 기록의 형식을 포멧화
+const formatting = (num) => {
+  return String(num).padStart(2, '0');
+};
 export default getDate;
