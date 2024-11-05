744c4c7020fb7a4efe79f42e3bfcc8b8df10baa2
goeun kim <rlarhdms6828@naver.com>
2024-11-05 10:15:00 +0900

    Feat: react router dom 설정

--- Week 04/assignment/src/App.tsx
+++ Week 04/assignment/src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <>1</>,
+    },
+    {
+      path: '/sign',
+      element: <>2</>,
+    },
+    { path: '/mypage', element: <>3</> },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
