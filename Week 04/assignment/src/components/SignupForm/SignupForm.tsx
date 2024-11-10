a80eea98eb14a140f4cad3832591a4f7f16bbfd2
goeun kim <rlarhdms6828@naver.com>
2024-11-10 17:17:08 +0900

    Feat: 회원가입 버튼 에러시 비활성화 처리

--- Week 04/assignment/src/components/SignupForm/SignupForm.tsx
+++ Week 04/assignment/src/components/SignupForm/SignupForm.tsx
@@ -1,20 +1,27 @@
 import { Input, Form } from '../../pages/Login/Login.style';
-import { Label } from './SignupForm.style';
+import { Label, SubmitBtn } from './SignupForm.style';
 
 type Info = {
   label: string;
   description: string;
   onNext: () => void; // 함수
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isDisabled: boolean;
+  errmeg: string;
 };
 
 // React.Fc  (함수형 컴포넌트의 약자)
 // <Info>는 전달 받는 props의 타입을 지정해줌
-const SignupForm: React.FC<Info> = ({ label, description, onNext }) => {
+const SignupForm: React.FC<Info> = ({ label, description, onNext, value, onChange, isDisabled, errmeg }) => {
   return (
     <Form>
       <Label>{label}</Label>
-      <Input placeholder={description}></Input>
-      <button onClick={onNext}>다음</button>
+      <Input disabled={isDisabled} placeholder={description} value={value} onChange={onChange}></Input>
+      {isDisabled && <span>{errmeg}</span>}
+      <SubmitBtn disabled={value.length === 0 || isDisabled} onClick={onNext}>
+        다음
+      </SubmitBtn>
     </Form>
   );
 };
--- Week 04/assignment/src/pages/Signup/Signup.tsx
+++ Week 04/assignment/src/pages/Signup/Signup.tsx
@@ -1,19 +1,63 @@
 import SignupForm from '../../components/SignupForm/SignupForm';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Wrapper } from '../Login/Login.style';
 import { Title, Footer } from './Signup.style';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
   const [step, setStep] = useState<'이름' | '비밀번호' | '취미' | '등록성공'>('이름');
+
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [hobby, setHobby] = useState<string>('');
+
+  // <input> 요소에서 발생하는 이벤트 -> React.ChangeEvent<HTMLInputElement>로 지정
+  const handleNamechange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handlepasswordchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  const handlehobbychange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHobby(e.target.value);
+  };
+
   return (
     <Wrapper>
       <Title>회원가입</Title>
-      {step == '이름' && <SignupForm label="이름" description="이름을 입력하세요" onNext={() => setStep('비밀번호')} />}
-      {step == '비밀번호' && (
-        <SignupForm label="비밀번호" description="비밀번호를 입력하세요" onNext={() => setStep('취미')} />
+      {step == '이름' && (
+        <SignupForm
+          label="이름"
+          description="이름을 입력하세요"
+          onNext={() => setStep('비밀번호')}
+          value={name}
+          onChange={handleNamechange}
+          isDisabled={name.length > 7}
+          errmeg="이름은 8자 이하로 입력해주세요"
+        />
       )}
-      {step == '취미' && <SignupForm label="취미" description="취미를 입력하세요" onNext={() => setStep('등록성공')} />}
+      {step == '비밀번호' && (
+        <SignupForm
+          label="비밀번호"
+          description="비밀번호를 입력하세요"
+          onNext={() => setStep('취미')}
+          value={password}
+          onChange={handlepasswordchange}
+          isDisabled={password.length > 7}
+          errmeg="비밀번호는 8자 이하로 입력해주세요"
+        />
+      )}
+      {step == '취미' && (
+        <SignupForm
+          label="취미"
+          description="취미를 입력하세요"
+          onNext={() => setStep('등록성공')}
+          value={hobby}
+          onChange={handlehobbychange}
+          isDisabled={hobby.length > 7}
+          errmeg="취미는 8자 이하로 입력해주세요"
+        />
+      )}
       <Footer>
         아차차, 이미 회원이신가요?
         <Link to="/">
